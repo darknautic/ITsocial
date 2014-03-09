7dfe41311b0a253300c8dadd94f7e8dc992a0cd0
darknautic <blue.darknautic@gmail.com>
2014-03-09 14:50:07 -0600

    some basic files.

--- js/event-object.js
+++ js/event-object.js
@@ -1,35 +1,41 @@
 
 	function readyFunc( jQuery ) {
-		// Code to run when the document is ready.
+		// code to run when the document is ready.
 		console.log("JQuery Loaded .");		
+
+		
 		$(".updateEventBox").on('click', function() {
 		
 		 console.log("click on div");
 
-
 		});
+		
 
-        $('.editable').on('activate', function() {
-            console.log('active');
-            $(this).empty();
-            var range, sel;
-            if ( (sel = document.selection) && document.body.createTextRange) {
-                range = document.body.createTextRange();
-                range.moveToElementText(this);
-                range.select();
-            }
-        });
 
-        $('.editable').focus(function() {
-            if (this.hasChildNodes() && document.createRange && window.getSelection) {
-                $(this).empty();
-                var range = document.createRange();
-                range.selectNodeContents(this);
-                var sel = window.getSelection();
-                sel.removeAllRanges();
-                sel.addRange(range);
-            }
-        });
+		$(".eventObject").on('click',function(){
+			//$("#eventContent").empty();
+			//$("#eventContent").append("<div style=\"border: 1px solid #0000FF; position:relative;  min-height: 100%; height: auto !important; height: 100%;\"  ></div>");
+			
+			console.log("click");
+			
+			});
+
+
+
+		
+		$('div').focus(
+		function(){
+		    //$(this).css({'background-color' : '#EDEDED'});
+		    //$(".eventObject").css({'background-color' : '#EDEDED'});
+		    
+		});
+		/*
+		$('div').blur(
+		function(){
+		    $(this).css({'background-color' : '#DFD8D1'});
+		});
+		*/
+
 
 		} //main function
 
